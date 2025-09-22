8f7ceb95df024a84018afe0e6586e40eb96c1eac
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2025-09-22 13:18:01 +0900

    ㅇㄹㄴ

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allow requests from this origin
-    origin: true, 
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe({
-  whitelist: true,
-  transform: true,
-  transformOptions: { enableImplicitConversion: true }, 
-  forbidNonWhitelisted: true,
-}));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const uploadsRoot = join(process.cwd(), 'uploads');
   const videoDir = join(uploadsRoot, 'video');
@@ -26,6 +28,6 @@ async function bootstrap() {
   // 정적 파일 매핑: /video/** → uploads/video/**
   app.use('/video', express.static(videoDir));
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // ← 이 형태로
 }
 bootstrap();
