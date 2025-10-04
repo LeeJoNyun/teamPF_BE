cfa4075f089bf83f7ea7ab035239a1b75cc894ee
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2025-10-04 14:52:27 +0900

    수정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import * as express from 'express';
 import * as fs from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // JSON body 용량 늘리기
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
   app.enableCors({
     // origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allow requests from this origin
     origin: true,
